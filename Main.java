3f04840517a1474ecd38f29f79accf7e91e302ff
Georky <georky27condoy@gmail.com>
2018-12-03 17:38:08 -0500

    Modificado Constructor

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        Metodos met = new Metodos(10, 50);
+        Metodos met = new Metodos(10, 60);
         met.suma();
     }
 }
