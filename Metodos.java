aa6e712014ebadc0d515e693df7ae1ffe1b7c7cc
Georky <georky27condoy@gmail.com>
2018-12-03 17:29:00 -0500

    Añadido Metodos.java

---
+++ Metodos.java
@@ -0,0 +1,17 @@
+
+public class Metodos {
+int num1;
+int num2;
+
+    public Metodos(int num1, int num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    
+    public void suma ()
+    {
+        int total = num1+num2;
+        System.out.println("la suma es"+total);
+    }
+}
