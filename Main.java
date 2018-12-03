d47df38c720e1dc289115ce0f23485bef18f65bf
Georky <georky27condoy@gmail.com>
2018-12-03 18:43:09 -0500

    Modificados main metodos

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         Metodos met = new Metodos(10, 800);
         met.suma();
-        met.resta();
+        met.Resta();
+        met.Multiplicacion();
     }
 }
--- Metodos.java
+++ Metodos.java
@@ -12,11 +12,17 @@ int num2;
     public void suma ()
     {
         int total = num1+num2;
-        System.out.println("la suma es"+total);
+        System.out.println("la suma es de dos numeros"+total);
     }
-    public void resta()
+     public void Resta ()
     {
         int total = num1-num2;
-        System.out.println("la Resta es"+total);
+        System.out.println("la Resta "+total);
     }
+      public void Multiplicacion ()
+    {
+        int total = num1*num2;
+        System.out.println("la Multiplicacion "+total);
+    }
+     
 }
